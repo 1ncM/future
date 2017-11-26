e14b4f201f21d72c3ebcb59e22ed6c311584c6ca
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2017-11-26 21:50:49 +0300

    added action create to railway_carriages_controller

--- app/controllers/railway_carriages_controller.rb
+++ app/controllers/railway_carriages_controller.rb
@@ -6,4 +6,20 @@ class RailwayCarriagesController < ApplicationController
   def new
   	@railway_carriage = RailwayCarriage.new
   end
+
+  def create
+	@railway_carriage = RailwayCarriage.new(railway_carriage_params)
+
+	if @railway_carriage.save
+	  redirect_to @railway_carriage
+	else
+	  render :new
+	end
+  end
+
+  private
+
+    def railway_carriage_params
+      params.require(:railway_carriage).permit(:number, :train_id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124205200) do
+ActiveRecord::Schema.define(version: 20171126184804) do
 
   create_table "railway_carriages", force: :cascade do |t|
     t.string "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "train_id"
+    t.index ["train_id"], name: "index_railway_carriages_on_train_id"
   end
 
   create_table "railway_stations", force: :cascade do |t|
