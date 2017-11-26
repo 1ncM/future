89562c82e020aefc20dcad32b3e2283ec1898034
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2017-11-26 21:39:44 +0300

    added action new, view new, partial form for railway_carriage

--- app/controllers/railway_carriages_controller.rb
+++ app/controllers/railway_carriages_controller.rb
@@ -2,4 +2,8 @@ class RailwayCarriagesController < ApplicationController
   def index
   	@railway_carriages = RailwayCarriage.all
   end
+
+  def new
+  	@railway_carriage = RailwayCarriage.new
+  end
 end
