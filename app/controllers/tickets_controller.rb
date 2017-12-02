6697819e904d3f84cdce5712edca24c645ad9fe8
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2017-12-02 22:44:40 +0300

    realize CRUD for ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,50 @@
 class TicketsController < ApplicationController
+	
 	def index
 		@tickets = Ticket.all
+	end
+
+	def show
+		@ticket = Ticket.find(params[:id])
+	end
+
+	def new
+		@ticket = Ticket.new
+	end
+
+	def create
+		@ticket = Ticket.new(ticket_params)
+
+		if @ticket.save
+			redirect_to @ticket
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@ticket = Ticket.find(params[:id])
+	end
+
+	def update
+		@ticket = Ticket.find(params[:id])
+		
+		if @ticket.update(ticket_params)
+			redirect_to @ticket
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@ticket = Ticket.find(params[:id])
+		@ticket.delete
+		redirect_to tickets_path
+	end
+
+	private
+
+	def ticket_params
+		params.require(:ticket).permit(:number, :fio, :first_station_id, :last_station_id)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201171439) do
+ActiveRecord::Schema.define(version: 20171202193129) do
 
   create_table "railway_carriages", force: :cascade do |t|
     t.string "number"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171201171439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "fio"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
