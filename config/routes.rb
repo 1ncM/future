e718b4d15b645bafdc1cb69b3c5767dfd1a0e454
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-02-01 00:05:03 +0300

    refactoring STI

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :routes
   resources :tickets
   resources :railway_carriages
+  resources :economy_carriages, controller: 'railway_carriages', type: 'Economy'
+  resources :coupe_carriages, controller: 'railway_carriages', type: 'Coupe'
+  resources :sedentary_carriages, controller: 'railway_carriages', type: 'Sedentary'
+  resources :sw_carriages, controller: 'railway_carriages', type: 'Sw'
   resources :users
   get 'welcome/index'
   root 'welcome#index'
