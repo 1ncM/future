19973622288fc61dbf833229efd620f68726be6d
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-02-09 18:43:04 +0300

    уникальность номера вагона у поезда

--- app/models/railway_carriage.rb
+++ app/models/railway_carriage.rb
@@ -1,3 +1,15 @@
 class RailwayCarriage < ApplicationRecord
+	validates :number, presence: true
+	validates :number, uniqueness: { scope: :train, message: "number already exists"}
+
 	belongs_to :train
+
+	before_validation :set_number
+
+	private
+
+	def set_number
+		self.number = self.class.count + self.train.number
+	end
+
 end
