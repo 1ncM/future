922f4116aecb547548351197f28288fdf55a5519
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-18 18:16:18 +0300

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'Alexandr',
+  :password => '123123',
+  :domain => 'floating-shelf-33966.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
