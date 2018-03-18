2e2239f7c2f48ee2ea8f7bf3bb584feb4202b133
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-18 17:56:48 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Thinknetica_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "floating-shelf-33966.herokuapp.com"}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
