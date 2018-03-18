0b32374eadda46c93f2d7c4d1acf65e3851f4e66
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-18 18:08:59 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,15 +76,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  ActionMailer::Base.smtp_settings = {
-    :user_name => 'Alexandr',
-    :password => '123123',
-    :domain => 'floating-shelf-33966.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :user_name => 'Alexandr',
+  #   :password => '123123',
+  #   :domain => 'floating-shelf-33966.herokuapp.com',
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
+  
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
