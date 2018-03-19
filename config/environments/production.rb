46b154147351336e8027c07138616adee27e817f
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-19 21:45:58 +0300

    last fi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Thinknetica_#{Rails.env}"
   config.action_mailer.perform_caching = false
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "floating-shelf-33966.herokuapp.com"}
+
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['app90809899@heroku.com'],
+    :password => ENV['d9r8gbkz9292'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
