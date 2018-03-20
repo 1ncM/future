9fceba63f966d38b70b11089ebd94933f82a21eb
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-20 23:41:19 +0300

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    user_name: 'apikey',
-    password: ENV['SENDGRID_API_KEY'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
     address: 'smtp.sendgrid.net',
     port: 587,
