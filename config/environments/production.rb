5de4c83f7668beebf7fcfd7acbe0ae31d82c5788
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-20 22:21:27 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,14 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+ActionMailer::Base.smtp_settings = {
+  :address          => "smtp.sendgrid.net",
+  :port             => "25",
+  :authentication   => :plain,
+  :user_name        => ENV['SENDGRID_USERNAME'],
+  :password         => ENV['SENDGRID_PASSWORD'],
+  :domain           => ENV['SENDGRID_DOMAIN']
+}
   config.action_mailer.default_url_options = { host: "floating-shelf-33966.herokuapp.com"}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
