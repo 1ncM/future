d3f63052d6fe2eeeda267660ff50e552bf855984
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-29 21:56:46 +0300

    Добавил инлайн редактирование станций

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	alert('Page loaded!');
+	$('.search_form').submit(function() {
+		let first_station;
+		let last_station;
+
+		first_station = $(this).find('#search_first_station_id');
+		last_station = $(this).find('#search_last_station_id');
+
+		console.log(last_station);
+		
+		if (first_station.val() == last_station.val()) {
+			alert('Выберите конечную станцию');
+			return false;
+		};
+	});
 });
