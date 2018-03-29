b84b42a001e22c1b1e7098adeca960bea70005b1
Александр Чупров <aleksandrcuprov@MBP-Aleksandr.Dlink>
2018-03-30 00:02:25 +0300

    custom turbolinks

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,12 +1,10 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
 	$('.search_form').submit(function() {
 		let first_station;
 		let last_station;
 
 		first_station = $(this).find('#search_first_station_id');
 		last_station = $(this).find('#search_last_station_id');
-
-		console.log(last_station);
 		
 		if (first_station.val() == last_station.val()) {
 			alert('Выберите конечную станцию');
